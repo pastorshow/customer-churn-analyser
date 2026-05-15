1e9dce3696280b611eb930f9d12cb080aed11d2f
Shoyemi Jonathan Showunmi <144295871+pastorshow@users.noreply.github.com>
2026-05-15 19:44:36 +0100

    Delete src directory

--- src/data_loader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from src.config import DATA_PATH
-
-def load_data():
-    df = pd.read_csv(DATA_PATH)
-    df = df.drop("CustomerID", axis=1)
-    return df
--- src/predict.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from src.config import MODEL_PATH
-
-def load_model():
-    with open(MODEL_PATH, "rb") as f:
-        return pickle.load(f)
-
-def predict(model, input_df):
-    return model.predict(input_df)
